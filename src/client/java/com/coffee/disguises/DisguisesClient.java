237029d109d89250922f7de60cb9a2ab13b3f4b7
Anonymous <140204225+Trexler180@users.noreply.github.com>
2026-03-03 20:23:34 -0500

    Initial Commit

---
+++ src/client/java/com/coffee/disguises/DisguisesClient.java
@@ -0,0 +1,10 @@
+package com.coffee.disguises;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DisguisesClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
